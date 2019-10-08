ca4883197587b39bb49fba4fdf1044633b0d3b7b
TakuyaYoshimatsu <55725098+TakuyaYoshimatsu@users.noreply.github.com>
2019-10-09 01:08:45 +0900

    UpdateYoshimatsu

--- Mammoner/Assets/Hero/Script/HeroDirectionalAnimation.cs
+++ Mammoner/Assets/Hero/Script/HeroDirectionalAnimation.cs
@@ -8,10 +8,14 @@ public class HeroDirectionalAnimation : MonoBehaviour
     public Animator Anim { get { return this._anim ? this._anim : this._anim = GetComponent<Animator>(); } }
     public Vector2 Direction { get; private set; }
 
+    //add start
+    private Vector3 prev_h;
+    private Vector3 prev_v;
+    //add end
+
     // Start is called before the first frame update
     void Start()
     {
-
     }
 
     // Update is called once per frame
@@ -20,10 +24,15 @@ public class HeroDirectionalAnimation : MonoBehaviour
         Vector2 inputAxis = new Vector2((int)Input.GetAxis("Horizontal"), (int)Input.GetAxis("Vertical"));
         if (inputAxis != Vector2.zero) Direction = inputAxis;
 
-        if (Input.GetKeyDown(KeyCode.W)) Direction = Vector2.up;
-        if (Input.GetKeyDown(KeyCode.A)) Direction = Vector2.left;
-        if (Input.GetKeyDown(KeyCode.S)) Direction = Vector2.down;
-        if (Input.GetKeyDown(KeyCode.D)) Direction = Vector2.right;
+        //add start yoshimatsu
+        var Horizontal = transform.position.x - prev_h.x;
+        var Vertical   = transform.position.y - prev_v.y;
+        //add end yoshimatsu
+
+        if (Vertical   > 0) Direction = Vector2.up;
+        if (Horizontal < 0) Direction = Vector2.left;
+        if (Vertical   < 0) Direction = Vector2.down;
+        if (Horizontal > 0) Direction = Vector2.right;
 
         if (Input.GetKeyUp(KeyCode.W) || Input.GetKeyUp(KeyCode.A) ||
             Input.GetKeyUp(KeyCode.S) || Input.GetKeyUp(KeyCode.D)) Direction = GetButtonDirection();
@@ -38,6 +47,11 @@ public class HeroDirectionalAnimation : MonoBehaviour
         {
             Anim.speed = 0.0f;
         }
+
+        //add start yoshimatsu
+        prev_h.x = transform.position.x;
+        prev_v.y = transform.position.y;
+        //add end yoshimatsu
 
     }
 
--- Mammoner/Assets/Hero/Script/InputMove.cs
+++ Mammoner/Assets/Hero/Script/InputMove.cs
@@ -5,25 +5,22 @@ using UnityEngine;
 public class InputMove : MonoBehaviour
 {
     [SerializeField]
-    float speed = 1.0f;
+    float speed = 0.1f;
     Vector2 inputAxis;
-    private Rigidbody2D _rig;
-    public Rigidbody2D Rig { get { return this._rig ? this._rig : this._rig = GetComponent<Rigidbody2D>(); } }
+    //private Rigidbody2D _rig;
+    //public Rigidbody2D Rig { get { return this._rig ? this._rig : this._rig = GetComponent<Rigidbody2D>(); } }
+    Rigidbody2D rb;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        inputAxis.x = Input.GetAxis("Horizontal");
-        inputAxis.y = Input.GetAxis("Vertical");
-    }
-    void FixedUpdate()
-    {
-        Rig.MovePosition(Rig.position + inputAxis * speed);
+        var v = new Vector2(-speed, rb.velocity.y);
+        transform.Translate(v);
     }
 }
